e4dc720461ee81d48771121b0e9fe156c8e791f0
haster12345 <tarunmankani25@gmail.com>
2024-01-30 20:59:31 +0000

    working pre_flop_parser with pre_flop.json as example

--- pre_flop_info.py
+++ pre_flop_info.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 
@@ -12,9 +13,9 @@ class PreFlop:
             pokerstars_file_content = file.read()
         return pokerstars_file_content
 
-    def hand_numbers(self, text):
+    def hand_numbers(self):
         pattern = re.compile(r'PokerStars Hand #(\d+):')
-        match = pattern.findall(text)
+        match = pattern.findall(self.pokerstars_file_content)
         match = [int(i) for i in match]
         return match
 
@@ -24,21 +25,11 @@ class PreFlop:
 
         return pre_flops
 
-    def get_hand(self):
-        
-        pre_flops = self.get_pre_flop()
 
-        hands = []
-
-        for pre_flop in pre_flops:
-
-            hand_number = self.hand_numbers(pre_flop)
-            pre_flop_re = re.compile(r"Dealt to (\w+) \[([2-9TJQKA][cdhs] [2-9TJQKA][cdhs])\]")
-            hand = pre_flop_re.findall(self.pokerstars_file_content)
-            hands.append((hand, hand_number))
-
-        return hands
-
+    def get_hand(self, pre_flop):
+        pre_flop_re = re.compile(r"Dealt to (\w+) \[([2-9TJQKA][cdhs] [2-9TJQKA][cdhs])\]")
+        hand = pre_flop_re.findall(pre_flop)
+        return hand
 
     def get_position(self, hand_number):
         """
@@ -61,9 +52,8 @@ class PreFlop:
         """
         example output: [x c] = Check call
         """
-        pattern = re.compile(r'hastermaster:\s*(.*)')
+        pattern = re.compile(r'(\S+):\s*(.*)')
         matches = pattern.findall(text)
-        
         return matches
 
     def get_pot_size(self):
@@ -73,22 +63,38 @@ class PreFlop:
     
     def json_builder(self):
 
-        for hand_number in self.hand_numbers():
-            
-            hand, hand_number = self.get_hand()
+        print(len(self.get_pre_flop()))
+        print(len(self.hand_numbers()))
 
+        jsons = []
+
+        for i, pre_flop in enumerate(self.get_pre_flop()):
+            print(pre_flop)
+            hand_number = self.hand_numbers()[i]
+            print(hand_number)
+            hand = self.get_hand(pre_flop)
+
+            
             json = {
-                'hand_number' : int(hand_number),
+                'hand_number' : hand_number,
                 'hand': hand,
-                'position': self.get_position(),
-                'action' : self.get_action(),
+                'position': self.get_position(hand_number),
+                'action' : self.get_action(pre_flop),
                 'players_in_pot' : self.get_palyers_in_pot(),
                 'pot_size' : self.get_pot_size(),
-                'villain_positions' : self.get_villain_position()
+                'villain_positions' : self.get_villain_position(hand_number)
             }                        
 
-        return json
+            jsons.append(json)
+
+        return jsons
+
+    def parse_into_json(self):
+        with open('pre_flop.json', 'w') as fp:
+            json_string = json.dumps(self.json_builder(), default=lambda o:__dict__, indent=2)
+            fp.write(json_string)
 
 
-inst = PreFlop(file_path="hastermaster/HH20231118 Aigyptios - $0.05-$0.10 - USD No Limit Hold'em.txt")
-print(inst.get_pre_flop())
+
+inst = PreFlop(file_path="hastermaster/HH20231118_Aigyptios_-_0.05-0.10_-_USD_No_Limit_Holdem.txt")
+inst.parse_into_json()
