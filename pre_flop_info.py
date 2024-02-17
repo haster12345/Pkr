20cfb4169f72b343ba9865f2782c1ef2011fc113
haster12345 <tarunmankani25@gmail.com>
2024-02-17 20:40:46 +0000

    clean up

--- pre_flop_info.py
+++ pre_flop_info.py
@@ -5,14 +5,7 @@ import re
 class PreFlop:
 
     def __init__(self, file_content):
-        # self.file_path = file_path
         self.file_content = file_content
-
-    # @property
-    # def pokerstars_file_content(self):
-    #     with open(self.file_path, 'r') as file:
-    #         pokerstars_file_content = file.read()
-    #     return pokerstars_file_content
 
     def hand_numbers(self):
         pattern = re.compile(r'PokerStars Hand #(\d+):')
@@ -20,14 +13,13 @@ class PreFlop:
         match = [int(i) for i in match]
         return match
 
-    def get_pre_flop(self) -> list:
+    def pre_flops(self) -> list:
         pattern = re.compile(r'HOLE CARDS([\s\S]*?)(?:FLOP|SUMMARY)')
         pre_flops = pattern.findall(self.file_content)
 
         return pre_flops
 
-
-    def get_hand(self, pre_flop):
+    def hand_dealt(self, pre_flop):
         pre_flop_re = re.compile(r"Dealt to (\w+) \[([2-9TJQKA][cdhs] [2-9TJQKA][cdhs])\]")
         hand = pre_flop_re.findall(pre_flop)
         return hand
@@ -49,39 +41,44 @@ class PreFlop:
     def get_palyers_in_pot(self):
         pass
 
-    def get_action(self, text):
+    def action(self, pre_flop):
         """"
-        output ideas:
-        1) action : [c f r f f c f]
-            -  this doesnt provide with much info by itself, everything must be implied
-            - maybe this is better as notation is more comapct but needs functions to map actions to players
-              and values for raises and calls is needed
-        2) action: [['A', 'checks'], ['B', 'raises']]
+        output:
+        FUTURE:
+          action : [c f r f f c f]
+            - easier to query, requires keeping track of players, player positions, raise and bet amounts
+        CURRENT: 
+          action: [['A', 'checks'], ['B', 'raises']] - current
             - same as above but now names are added and more information about actions
         """
         pattern = re.compile(r'(\S+):\s*(.*)')
-        action_player_list = pattern.findall(text)
-        actions = []        
+        action_player_list = pattern.findall(pre_flop)
         return action_player_list
 
-    def get_pot_size(self):
+    def get_pot_size(self, blinds):
         """
+        -keep track of blinds, bets, raises, calls
+        iterate through actions, 
+
         """
+        pot = blinds
+
+
         pass
     
     def json_builder(self):
 
         jsons = []
 
-        for i, pre_flop in enumerate(self.get_pre_flop()):
+        for i, pre_flop in enumerate(self.pre_flop()):
             hand_number = self.hand_numbers()[i]
-            hand = self.get_hand(pre_flop)
+            hand = self.hand_dealt(pre_flop)
             
             json = {
                 'hand_number' : hand_number,
                 'hand': hand,
                 'position': self.get_position(hand_number),
-                'action' : self.get_action(pre_flop),
+                'action' : self.action(pre_flop),
                 'players_in_pot' : self.get_palyers_in_pot(),
                 'pot_size' : self.get_pot_size(),
                 'villain_positions' : self.get_villain_position(hand_number)
