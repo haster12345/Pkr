a04494640356882015c37aa2344c4ef1827cddfc
haster12345 <tarunmankani25@gmail.com>
2024-04-15 22:24:06 +0100

    cleanup

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-from populate_tables import populate_table_all_files
+from populate_tables import populate_table
 
 
 def main(folder_path):
     """
     iterate through a directory and pass each file into the parser
     """
-    return populate_table_all_files(folder_path)
+    return populate_table(folder_path)
 
 if __name__ == '__main__':
     main('../Samples/hastermaster/')
--- src/populate_tables.py
+++ src/populate_tables.py
@@ -5,21 +5,23 @@ import os
 from local_db import dynamodb
 
 
-def populate_table_all_files(folder_path):
+def concat_hands(folder_path):
     """
     iterate through a directory and pass each file into the parser
     """
     item_list = []
 
     for filename in os.listdir(folder_path):
+
         if filename[-3:]  == 'txt':
             hands = FileContent(folder_name=folder_path,file_name=filename).hands()
             items = PokerStarsParser(hands).parse()[2]
             item_list += items
 
-    return populate_table(item_list) 
+    return item_list
 
-def populate_table(item_list):
+def populate_table(folder_path):
+    item_list = concat_hands(folder_path)
     table = dynamodb.Table('hand_info')
     with table.batch_writer() as batch:
         for item in item_list:
