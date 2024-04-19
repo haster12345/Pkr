dec5294012d4e78a4e37a95352777b16cf98d0d5
haster12345 <tarunmankani25@gmail.com>
2024-04-19 15:32:59 +0100

    clean up

--- src/main.py
+++ src/main.py
@@ -3,9 +3,6 @@ from populate_tables import populate_table
 
 
 def main(folder_path):
-    """
-    iterate through a directory and pass each file into the parser
-    """
     return populate_table(folder_path)
 
 if __name__ == '__main__':
